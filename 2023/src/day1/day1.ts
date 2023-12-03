373d0aa3dc6ef22a233ffdeb45fa6e2819ce6f1d
DaanGrob05 <d.grob05@gmail.com>
2023-12-03 14:51:52 +0100

    first star achieved

--- 2023/src/day1/day1.ts
+++ 2023/src/day1/day1.ts
@@ -1,6 +1,21 @@
 import * as fs from "fs";
 
-const text = fs.readFileSync("./input.txt", "utf-8");
+const lines = fs.readFileSync("./input.txt", "utf-8").split("\n");
 
-console.log(text);
+const extractNumbers = (text: string) => {
+  return text.replace(/[^0-9]/g, "");
+};
 
+const getCalibrationValue = (text: string): number => {
+  if (text.length === 1) return +(text + text);
+
+  return +(text.charAt(0) + text.slice(-1));
+};
+
+let total = 0;
+
+lines.forEach((line) => {
+  total += getCalibrationValue(extractNumbers(line));
+});
+
+console.log(total);
